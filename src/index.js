18c4eaabe2a6eee525641ec55b52a951d16cf6ef
Paulo Marcelo <marcelomp97447@gmail.com>
2025-12-05 20:23:33 -0300

    e pra funcionar2.0

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
   res.send("API funcionando!");
 });
 
+app.get("/feijao", (req, res) => {
+  res.send("feijao preto!");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
